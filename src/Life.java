ce1ddefe1f97287bf87d42f1ec9e6b49a1876099
Parkseho <sep775@naver.com>
2021-06-15 12:54:17 +0900

    Life

--- src/Life.java
+++ src/Life.java
@@ -1,19 +1,81 @@
-
+import java.text.DecimalFormat;
+ 
 public class Life {
-	String Tasks;
-	String Name;
+    
+    private int personNum;
+    private String personName;
+    private String task1;
+    private String task2;
+    private String task3;    
+    private String task4;    
+    private String task5;    
 
-	public Life() {}
+    public Life(int personNum, String personName, String task1, String task2, String task3, String task4, String task5) {
+        super();
+        this.personNum = personNum;
+        this.personName = personName;
+        this.task1 = task1;
+        this.task2 = task2;
+        this.task3 = task3;
+        this.task4 = task4;
+        this.task5 = task5;
 
-	public Life(String name, String tasks) {
-		super();
-		Name = name;
-		Tasks = tasks;
+    }
+
+	public int getPersonNum() {
+		return personNum;
 	}
 
-	
-	public void printInof() {
-		System.out.println("Name: "+ Name + "Tasks: "+ Tasks);	
+	public void setPersonNum(int personNum) {
+		this.personNum = personNum;
 	}
-	
+
+	public String getPersonName() {
+		return personName;
+	}
+
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+
+	public String getTask1() {
+		return task1;
+	}
+
+	public void setTask1(String task1) {
+		this.task1 = task1;
+	}
+
+	public String getTask2() {
+		return task2;
+	}
+
+	public void setTask2(String task2) {
+		this.task2 = task2;
+	}
+
+	public String getTask3() {
+		return task3;
+	}
+
+	public void setTask3(String task3) {
+		this.task3 = task3;
+	}
+
+	public String getTask4() {
+		return task4;
+	}
+
+	public void setTask4(String task4) {
+		this.task4 = task4;
+	}
+
+	public String getTask5() {
+		return task5;
+	}
+
+	public void setTask5(String task5) {
+		this.task5 = task5;
+	}
+
 }
